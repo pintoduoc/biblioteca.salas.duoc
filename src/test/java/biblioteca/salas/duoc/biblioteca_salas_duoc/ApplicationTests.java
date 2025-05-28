69fdab9d38674d00c06209b09e81774e3db17158
benja <be.pintor@duocuc.cl>
2025-05-28 10:02:04 -0400

    a

---
+++ src/test/java/biblioteca/salas/duoc/biblioteca_salas_duoc/ApplicationTests.java
@@ -0,0 +1,13 @@
+package biblioteca.salas.duoc.biblioteca_salas_duoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
